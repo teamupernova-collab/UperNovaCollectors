4643ecef3eb24bd0c47b0f1f373b6979eeb2e3a0
jorgeperez <jorgearmandop.rivera@gmail.com>
2025-09-18 16:15:26 -0600

    add colors and typography

--- app/src/main/java/com/upernova/collectors/ui/theme/Color.kt
+++ app/src/main/java/com/upernova/collectors/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package com.upernova.collectors.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryRed = Color(0xFFE85C50)
+val PrimaryRedLight = Color(0xFFFF8A7E)
+val PrimaryRedDark = Color(0xFFB23328)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BackgroundBeige = Color(0xFFE8D9C1)
+val BackgroundBeigeLight = Color(0xFFFFF2DE)
+val BackgroundBeigeDark = Color(0xFFC7B896)
+
+val CardGray = Color(0xFF6A6663)
+val CardGrayLight = Color(0xFF8E8A87)
+val CardGrayDark = Color(0xFF494644)
+
+val SurfaceWhite = Color(0xFFFFFFFF)
+val SurfaceWhiteLight = Color(0xFFFFFFFF)
+val SurfaceWhiteDark = Color(0xFFEBEBEB)
+
+val TextDark = Color(0xFF2C2C2C)
+val TextDarkLight = Color(0xFF4D4D4D)
+val TextDarkDark = Color(0xFF000000)
+
+val TextLight = Color(0xFF9A9A9A)
+val TextLightLight = Color(0xFFBFBFBF)
+val TextLightDark = Color(0xFF7A7A7A)
+
+val DividerGray = Color(0xFFDDDDDD)
+val DividerGrayLight = Color(0xFFF2F2F2)
+val DividerGrayDark = Color(0xFFBFBFBF)
--- app/src/main/java/com/upernova/collectors/ui/theme/Theme.kt
+++ app/src/main/java/com/upernova/collectors/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.upernova.collectors.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,28 +8,35 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+val DarkColorScheme = darkColorScheme(
+    primary = PrimaryRedLight,
+    onPrimary = Color.Black,
+
+    background = CardGrayDark,
+    onBackground = Color.White,
+
+    surface = CardGray,
+    onSurface = Color.White,
+
+    secondary = BackgroundBeigeDark,
+    onSecondary = Color.Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = PrimaryRed,
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+
+    background = BackgroundBeige,
+    onBackground = TextDark,
+
+    surface = SurfaceWhite,
+    onSurface = TextDark,
+
+    secondary = CardGray,
+    onSecondary = Color.White
 )
 
 @Composable
